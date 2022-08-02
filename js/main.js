ab679dfdbae8de90f6cfe6cbd56b2d8365456ffe
MaxLocal <maxlocal@gmail.com>
2022-08-02 16:49:58 +0300

    Добавлена функция рандомайзер

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 "use strict";
 
-let likesMin = 15;
-let likesMax = 200;
+const QUANTITY_PICTURES = 25;
 
-let description = [];
+const LIKES = {
+    min: 15,
+    max: 200
+};
 
-let comments = [
+const COMMENTS = [
     'Всё отлично!',
     'В целом всё неплохо. Но не всё.',
     'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -14,3 +16,23 @@ let comments = [
     'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
+const DESCRIPTION = [];
+
+let names = [
+    'Jhon',
+    'Mike',
+    'April',
+    'Sara',
+    'Mka',
+    'Steve',
+];
+
+let fragment = document.createDocumentFragment();
+let pictureContainer = document.querySelector('#picture').content;
+let pictureTemplate = document.querySelector('.pictures');
+let bigPicture = document.querySelector('.big-picture');
+
+//* Получение рандомного индекса
+let randomIndex = function(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+};
